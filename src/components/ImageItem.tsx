06ed8f9f4adf6ef886a307c2f66b99addb764700
Soohyeon Kim <tngusglaso@gmail.com>
2023-03-03 02:46:18 +0900

    Feat: edit 기능 추가

--- src/components/ImageItem.tsx
+++ src/components/ImageItem.tsx
@@ -1,36 +1,38 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 import { useAppSelector, useAppDispatch } from '../app/hooks';
-import { select } from "../app/slices/selections";
+import { select } from '../app/slices/selections';
 
 const ImgPreview = styled.img`
   position: relative;
   left: 50%;
   transform: translateX(-50%);
-  width: 100%;  
+  width: 100%;
   background: #a70f11;
 `;
 
 type ImageProps = {
-    rowNumber: number;
-    id: number;
-    url: string,
+  rowNumber: number;
+  id: number;
+  url: string;
 };
 
+const ImagesItem = (props: ImageProps) => {
+  const dispatch = useAppDispatch();
+  const selections = useAppSelector((state) => state.selections).rows.find(
+    (selectionInRow) => selectionInRow.row === props.rowNumber
+  );
 
-const ImagesItem = (props : ImageProps) => {
-    const dispatch = useAppDispatch();
-    const selections = useAppSelector(state => state.selections).rows.find((selectionInRow) => selectionInRow.row == props.rowNumber);
-
-    const handleImgClick = (e: React.MouseEvent) => {
-        dispatch(select({row: props.rowNumber, id:props.id}));
-    };
-    return (
-        <div>
-            <ImgPreview src={props.url} onClick={handleImgClick} />
-        </div>
-    );
+  const handleImgClick = (e: React.MouseEvent) => {
+    dispatch(select({ row: props.rowNumber, id: props.id }));
+    // console.log(props.rowNumber);
+  };
+  return (
+    <div>
+      <ImgPreview src={props.url} onClick={handleImgClick} />
+    </div>
+  );
 };
 
 export default ImagesItem;
